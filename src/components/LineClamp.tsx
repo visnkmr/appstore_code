6c9dd7e334268eb9cb2940d01279a2eebc20200e
12core1 <12533753+visnkmr@users.noreply.github.com>
2023-05-20 22:34:07 +0530

    lc extend htmldiv

--- src/components/LineClamp.tsx
+++ src/components/LineClamp.tsx
@@ -1,15 +1,14 @@
 'use client';
+import _ from 'lodash';
 import { useState, useEffect, useRef } from 'react';
 import classnames from 'classnames';
-import _ from 'lodash';
 
 interface LineClampProps extends React.HTMLAttributes<HTMLDivElement> {
-  // className:string|undefined,
   text: string;
   lines?: number;
 }
 
-const LineClamp = ({ text, lines = 2 }: LineClampProps) => {
+const LineClamp = ({ text, lines = 2, className, ...props }: LineClampProps) => {
   const [clamped, setClamped] = useState(true);
   const [showButton, setShowButton] = useState(false);
   const containerRef = useRef<HTMLDivElement>(null);
@@ -35,10 +34,8 @@ const LineClamp = ({ text, lines = 2 }: LineClampProps) => {
   };
 
   return (
-    <div className="relative">
-      <div ref={containerRef} className={classnames(clamped ? `line-clamp-${lines}` : "")} onClick={toggleClass}>
-        {text}
-      </div>
+    <div className={classnames("line-clamp", `line-clamp-${lines}`, clamped ? "clamp" : "not-clamp", className)} ref={containerRef} onClick={toggleClass} {...props}>
+      {text}
       {showButton && (
         <button onClick={handleClick} className="absolute right-0 bottom-0 bg-white text-blue-500 hover:text-blue-700">
           {clamped ? "..." : "<See Less"}
