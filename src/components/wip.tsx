8010b98a59da591e5b29cd01c1c2a4f5360601d9
12core1 <12533753+visnkmr@users.noreply.github.com>
2023-05-23 16:08:08 +0530

    update work in progress component

--- src/components/wip.tsx
+++ src/components/wip.tsx
@@ -1,5 +1,32 @@
 import React from "react";
-
+import { findLatestapps } from "../posts";
+import LineClamp from "./LineClamp";
+import StoreIcons from "./storeicons";
+async function appsfetcher() {
+    const apps = await findLatestapps("projects/inp");
+    return (
+      <>
+    {apps.map(({ slug, title, image,tags, content,excerpt,download }: { 
+      slug: string, title: string, image: string, tags:Array<string>,content:string, excerpt:string,download:string }) => {
+        return ( 
+            <div
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+            <a 
+            className={`mb-3 text-2xl font-semibold`}
+            href=""
+            target="_blank"
+            >
+                {title}
+            </a>
+             <LineClamp className="m-0 max-w-[30ch] text-sm opacity-50" text={content} lines={2}/>
+             <StoreIcons storename={tags} w={1}/>
+        </div>
+        );
+    })}
+    </>
+    );
+  }
 export default function Workinp(){
     return (
         <>
@@ -7,38 +34,7 @@ export default function Workinp(){
         <div className='font-bold mb-4 leading-tighter tracking-tighter md:px-0 md:text-[2.48rem] px-4 text-5xl m-5'>Recent Projects</div>
 
         <div className="mb-8 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-            href=""
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-        >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-            Iomer{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-            </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            File explorer made using rust and tauri. Support multi-window, tabs, fzf like search, swift folder size compute, hot reload for markdown, html.
-            </p>
-        </a>
-
-        <a
-            href=""
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-            Prefstore{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-            </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Rust lib to save preferences with one line of code.
-            </p>
-        </a>
+        {appsfetcher()}
         </div>
         </div>
 
