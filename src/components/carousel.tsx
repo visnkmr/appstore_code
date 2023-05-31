2bffdb80dc1951cd2235ed1ea401b19e0a4872fa
12core1 <12533753+visnkmr@users.noreply.github.com>
2023-05-31 22:36:19 +0530

    link webpage to images

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -15,19 +15,33 @@ export default function Caro(){
 
             <div>
 
-            <div className="screen1">
-                <img src="https://cdn.jsdelivr.net/gh/visnkmr/hv2static@main/gp.png"></img>
-            </div>
-            </div><div>
+                <div className="screen1">
+                    <a href='https://play.google.com/store/apps/developer?id=Vishnu+N+K'
+                    rel="noopener" 
+                    target="_blank">
+                        <img  src="https://cdn.jsdelivr.net/gh/visnkmr/hv2static@main/gp.png"></img>
+                    </a>
+                </div>
+                </div><div>
 
-            <div className="screen2">
-                <img src="https://cdn.jsdelivr.net/gh/visnkmr/hv2static@main/amz.png"></img>
-            </div>
-            </div><div>
+                <div className="screen2">
+                <a href='https://www.amazon.com/gp/mas/dl/android?p=io.github.visnkmr.bapl&showAll=1'
+                    rel="noopener" 
+                    target="_blank">
+                    <img src="https://cdn.jsdelivr.net/gh/visnkmr/hv2static@main/amz.png"></img>
+                </a>
+                </div>
 
-            <div className="screen3">
-                <img src="https://cdn.jsdelivr.net/gh/visnkmr/hv2static@main/gho.png"></img>
             </div>
+            <div>
+
+                <div className="screen3">
+                    <a href='https://github.com/visnkmr'
+                    rel="noopener" 
+                    target="_blank">
+                        <img src="https://cdn.jsdelivr.net/gh/visnkmr/hv2static@main/gho.png"></img>
+                    </a>
+                </div>
             </div>
         </Carousel>
         </div>
