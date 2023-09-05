20a58bee2244d28045fcc04ddd50be41d9c5a588
12core1 <12533753+visnkmr@users.noreply.github.com>
2023-09-05 12:57:12 +0530

    add commits history to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,6 +3,7 @@ export default function  Navbar(){
     <nav>
         <a>Home</a>
         <a>Android</a>
+        <a href="https://github.com/search?o=desc&s=committer-date&type=Commits&q=author%3Avisnkmr">Commits History</a>
         <a>PC</a>
         <a>Fire OS</a>
     </nav>
