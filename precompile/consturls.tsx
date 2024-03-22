757939e933416b73dabaab1da2c0295cf5c6b5c2
12core1 <12533753+visnkmr@users.noreply.github.com>
2024-03-23 01:16:07 +0530

    local dev

--- precompile/consturls.tsx
+++ precompile/consturls.tsx
@@ -1,3 +1,4 @@
 // export const asseturl="http://localhost:3000/";
-export const asseturl="https://cdn.jsdelivr.net/gh/visnkmr/appstore@main/";
+export const asseturl="/";
+// export const asseturl="https://cdn.jsdelivr.net/gh/visnkmr/appstore@main/";
 // using jsdelivr as cdn
