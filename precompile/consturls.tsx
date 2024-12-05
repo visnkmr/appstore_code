d8b1ea79237773360d2e0c038d27622d96ea9789
12core1 <12533753+visnkmr@users.noreply.github.com>
2024-12-05 22:38:23 +0530

    Update consturls.tsx

--- precompile/consturls.tsx
+++ precompile/consturls.tsx
@@ -1,2 +1,2 @@
-export const asseturl="";
+export const asseturl="https://cdn.jsdelivr.net/gh/visnkmr/appstore@main/";
 // export const asseturl="https://cdn.jsdelivr.net/gh/visnkmr/visnkmr.github.io@main/appstore/";
